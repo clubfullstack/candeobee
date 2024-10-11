a4e3158abcf0ab168ec1f20449f4acbfbd2bdf40
clubfullstack <165699267+clubfullstack@users.noreply.github.com>
2024-10-11 16:01:05 -0300

    Create script.js
    test ecovila sustentar

---
+++ script.js
@@ -0,0 +1,4 @@
+// Simples script de exemplo para interações futuras
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Página das Oficinas de Agrofloresta da Ecovila Sustentar carregada!");
+});
